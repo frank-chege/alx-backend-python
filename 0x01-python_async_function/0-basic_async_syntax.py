14d34dafbb88a41b870aebd2208bcdc2ce084f6f
frankrazzih <francischege602@gmail.com>
2024-05-06 23:05:04 +0300

    python backend

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,7 @@
 import asyncio
 import random
 async def  wait_random(max_delay=10):
+    '''returns the wait time'''
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time
