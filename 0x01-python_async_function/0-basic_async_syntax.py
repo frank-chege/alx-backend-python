ed35993cd9e33851c84ca7c26bf0099b620ef319
frankrazzih <francischege602@gmail.com>
2024-05-06 23:51:55 +0300

    python backend

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,7 @@
 '''using random library'''
 import asyncio
 import random
-async def  wait_random(max_delay=10):
+async def  wait_random(max_delay:int =10) ->float:
     '''returns the wait time'''
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
