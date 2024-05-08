4880e33cbd57e31e0a7f46d1365c2caced56cd3c
frankrazzih <francischege602@gmail.com>
2024-05-08 19:12:25 +0300

    python backend

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''create tasks'''
+import asyncio
+from typing import Any
+
+module = __import__('0-basic_async_syntax')
+wait_random = module.wait_random
+
+def task_wait_random(max_delay:int)-> Any:
+    '''returns an asyncio task'''
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
