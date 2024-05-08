59679bf26e23040e04165528e7a21a10bfb69e2b
frankrazzih <francischege602@gmail.com>
2024-05-08 12:09:39 +0300

    python backend

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,5 +5,4 @@ import random
 async def  wait_random(max_delay:int =10) ->float:
     '''returns the wait time'''
     wait_time = random.uniform(0, max_delay)
-    await asyncio.sleep(wait_time)
     return wait_time
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+import asyncio
+module = __import__('0-basic_async_syntax')
+wait_random = module.wait_random
+'''execute multiple coroutines at the same time'''
+
+async def wait_n(n, max_delay):
+    '''execute multiple coroutines at the same time'''
+    list = []
+    for x in range(n):
+        wait_time = await wait_random(max_delay)
+        list.append(wait_time)
+    return list
