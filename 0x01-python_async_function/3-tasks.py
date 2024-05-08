89a03893d338ed6bb5a7fc85243b983aeb44be5b
frankrazzih <francischege602@gmail.com>
2024-05-08 19:14:41 +0300

    python backend

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ from typing import Any
 module = __import__('0-basic_async_syntax')
 wait_random = module.wait_random
 
-def task_wait_random(max_delay:int)-> Any:
+def task_wait_random(max_delay:int)->Any:
     '''returns an asyncio task'''
     task = asyncio.create_task(wait_random(max_delay))
     return task
