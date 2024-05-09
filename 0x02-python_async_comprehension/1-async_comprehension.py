55657f3c7de74117f46d3d9d5c80f91d10526f06
frankrazzih <francischege602@gmail.com>
2024-05-09 22:55:50 +0300

    python backend async comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''using async comprehension'''
+from typing import List
 
 module = __import__('0-async_generator')
 async_generator = module.async_generator
 
-async def async_comprehension():
+async def async_comprehension()-> List[float]:
     '''using async comprehension'''
     return [random async for random in async_generator()]
