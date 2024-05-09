2583ab3a2bbbf1659c8f095ec8182593a2ef4896
frankrazzih <francischege602@gmail.com>
2024-05-09 23:49:11 +0300

    python backend async comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''run coroutines concurrently'''
+import asyncio
+import time
+from typing import 
+
+module = __import__('1-async_comprehension')
+async_comprehension = module.async_comprehension
+
+async def measure_runtime()->float:
+    tasks= []
+    for x in range(4):
+        tasks.append(async_comprehension())
+    start_time = time.time()
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    runtime = end_time - start_time
+    return runtime
