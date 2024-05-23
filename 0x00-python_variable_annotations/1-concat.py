3307631bc6edb46fc9156f43d4d3bc33227c58be
frankrazzih <francischege602@gmail.com>
2024-05-23 17:30:39 +0300

    python var annotation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 '''annotate  add function'''
-def add(str1: str, str2: str)->str:
+def concat(str1: str, str2: str)->str:
     '''annotate  function'''
     return str1 + str2
