958a3268987b80a68de8e6b8922b78d30a98e101
frankrazzih <francischege602@gmail.com>
2024-05-23 22:01:38 +0300

    python var annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''annotating lists with mixed types'''
-from typing import List
-def sum_mixed_list(mxd_lst: List[int, float])->float:
+from typing import List, Union
+def sum_mixed_list(mxd_lst: List[Union[int, float]])->float:
     '''annotating lists with mixed types'''
     return sum(mxd_lst)
